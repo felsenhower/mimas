a69b613fce75ccb485c145b8224e5a18ed5120e8
Ruben Felgenhauer <Ruben.Felgenhauer@posteo.de>
2025-09-22 13:59:19 +0200

    Add initialization js code

--- src/mimas/backend/serve_python_code.py
+++ src/mimas/backend/serve_python_code.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from pathlib import Path
 import mimas
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, Response
 from pydantic import BaseModel
 
 def walk_dir(p: Path):
@@ -24,6 +24,32 @@ class Overview(BaseModel):
     frontend_source_paths: list[str]
     frontend_extra_modules: list[str]
 
+init_js_code = """
+async function init_mimas() {
+    const pyodide = await loadPyodide();
+    async function get_overview() {
+        const response = await fetch("mimas/");
+        const json = await response.json();
+        return json
+    }
+    const overview = await get_overview();
+    for (let file_name of overview.frontend_source_paths) {
+        const parent_dir = file_name.substring(0, file_name.lastIndexOf("/"));
+        pyodide.FS.mkdirTree(parent_dir);
+        const response = await fetch("mimas/" + file_name);
+        const code = await response.text();
+        pyodide.FS.writeFile(file_name, code);
+    }
+    for (let module of overview.frontend_extra_modules) {
+        await pyodide.loadPackage(module);
+    }
+    const frontend_module = overview.frontend_module;
+    await pyodide.runPythonAsync(`import ${frontend_module}`);
+    const result = await pyodide.runPythonAsync(`${frontend_module}.main()`);
+}
+init_mimas();
+"""
+
 def make_app(frontend_module: str, frontend_source_paths, frontend_extra_modules):
     app = FastAPI()
     
@@ -34,6 +60,8 @@ def make_app(frontend_module: str, frontend_source_paths, frontend_extra_modules
     )
     
     app.add_api_route("/", endpoint=lambda: overview)
+    
+    app.add_api_route("/init.js", endpoint=lambda: Response(content=init_js_code, media_type="text/javascript"))
     
     file_locations = {
         str(target_path): source_path for (source_path, target_path) in get_frontend_python_dependencies(frontend_source_paths)
